89e4c75b6228eaff449f89e18a6d6f6e5c52de05
sultanBytes <sultankhand11@gmail.com>
2024-06-13 12:29:45 +0530

    Updates

---
+++ assets/index-MLTIJLQk.js
@@ -0,0 +1,1 @@
+import{G as a}from"./index-C2s6qTqW.js";function e(t){return a({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"},child:[]}]})(t)}export{e as M};
